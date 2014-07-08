597263acd0fb4dcb9d80597a19a24fcf3063467b
Rob Vatz <rob.vatz@sceneralabs.com>
2014-07-08 19:11:28 -0400

    Began fleshing out views
    Began cleaning up views to provide basic explanation.  Still need to add
    buttons.

---
+++ FeatureFlagExample.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Web.Mvc;
+using FeatureFlagExample.Controllers;
+using NUnit.Framework;
+
+namespace FeatureFlagExample.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Index()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result, "result should not be null");
+            Assert.That("Modify this template to jump-start your ASP.NET MVC application.", Is.EqualTo(result.ViewBag.Message));
+        }
+
+        [Test]
+        public void About()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        public void Contact()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+        }
+    }
+}
--- FeatureFlagExample.Tests/Properties/AssemblyInfo.cs
+++ FeatureFlagExample.Tests/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FeatureFlagExample")]
+[assembly: AssemblyTitle("FeatureFlagExample.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("FeatureFlagExample")]
+[assembly: AssemblyProduct("FeatureFlagExample.Tests")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9a045ac9-9730-411c-8d3a-deef7f8720d6")]
+[assembly: Guid("2fe8c86f-9d6b-4346-89be-ea6730e0901d")]
 
 // Version information for an assembly consists of the following four values:
 //
